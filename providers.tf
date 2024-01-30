aa89eef19749fe5f33125fd2662247503f885e1f
cupumelody <145847069+cupumelody@users.noreply.github.com>
2024-01-30 20:41:21 +0700

    #23 bucket should no longer use random provider (#24)

--- providers.tf
+++ providers.tf
@@ -13,11 +13,7 @@ terraform {
   # name = "terra-house-1"
   #  }
   #}
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
+  required_providers { 
     aws = {
       source = "hashicorp/aws"
       version = "5.34.0"
