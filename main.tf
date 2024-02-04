511af75fade95b698722e717fd7a22fca83a28b9
cupumelody duar <ilhamuttaqin666@gmail.com>
2024-02-04 19:45:27 +0000

    #44 test our terra home on terratowns test town

--- main.tf
+++ main.tf
@@ -22,20 +22,21 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
   
 }
-#module "terrahouse_aws" {
-#  source = "./modules/terrahouse_aws"
-#  user_uuid = var.user_uuid
-#  bucket_name = var.bucket_name
-#  index_html_filepath = var.index_html_filepath
-#  error_html_filepath = var.error_html_filepath
-#  content_version = var.content_version
-#  assets_path = var.assets_path
-#}
+
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
 resource "terratowns_home" "home" {
   name = "Berserk!"
@@ -47,8 +48,8 @@ and the dark consequences of power, exploring the depths of human nature and mor
 With intricate storytelling and visceral artwork, "Berserk" delves into the psychological complexities of its characters, 
 presenting a nightmarish landscape filled with grotesque creatures that challenge the boundaries of the human psyche.  
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "4sw23b.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "2adw2.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
